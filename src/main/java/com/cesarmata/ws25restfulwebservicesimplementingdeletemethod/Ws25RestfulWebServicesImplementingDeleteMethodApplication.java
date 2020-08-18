914922d07f49f08d239635cefe0518996c97ac3a
César <dev.pcesarmata@gmail.com>
2020-08-18 17:25:27 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws25restfulwebservicesimplementingdeletemethod/Ws25RestfulWebServicesImplementingDeleteMethodApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws25restfulwebservicesimplementingdeletemethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws25RestfulWebServicesImplementingDeleteMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws25RestfulWebServicesImplementingDeleteMethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws25restfulwebservicesimplementingdeletemethod/Ws25RestfulWebServicesImplementingDeleteMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws25restfulwebservicesimplementingdeletemethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws25RestfulWebServicesImplementingDeleteMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
